5bdd79ab6016a993d18cf26dc27455f6b0f13661
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-01-03 08:32:36 +0300

    created next-app width typescript extansion

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
+import {Box} from '../pages/box/Box';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Box/>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
